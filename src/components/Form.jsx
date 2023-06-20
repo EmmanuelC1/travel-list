b0ac266402efd17b0a53ea323a889c9b8f916416
EmmanuelC1 <emcastillo@csumb.edu>
2023-06-19 18:38:19 -0700

    adds conditional rendering, and makes new Item object based in user input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,43 @@
+import { useState } from 'react';
+
 export default function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // return if description if empty
+    if (!description) return;
+
+    // Make new Item object with inputs
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    // Reset input fields
+    setDescription('');
+    setQuantity(1);
+    document.activeElement?.blur(); // takes focus off input field
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select value={quantity} onChange={e => setQuantity(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
