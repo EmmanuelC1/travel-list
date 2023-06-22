5c8eed0cbf2e42cfa6f37f921aef7a427eb47f27
EmmanuelC1 <emcastillo@csumb.edu>
2023-06-21 21:17:04 -0700

    adds sorted by options, and functionality

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import Item from './Item';
+import Sort from './Sort';
 
 export default function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState('input');
+
+  const sortedItems =
+    sortBy === 'description'
+      ? items.slice().sort((a, b) => a.description.localeCompare(b.description))
+      : sortBy === 'packed'
+      ? items // sort by alphabetical and packed status
+          .slice()
+          .sort((a, b) => a.description.localeCompare(b.description))
+          .sort((a, b) => a.packed - b.packed)
+      : items; // sortBy = input ? then just original items arr
+
   return (
     <div className="list">
       <ul>
-        {items.map(item => (
+        {sortedItems.map(item => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
@@ -13,6 +27,8 @@ export default function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+
+      <Sort sortBy={sortBy} setSortBy={setSortBy} />
     </div>
   );
 }
---
+++ src/components/Sort.jsx
@@ -0,0 +1,11 @@
+export default function Sort({ sortBy, setSortBy }) {
+  return (
+    <div className="actions">
+      <select value={sortBy} onChange={e => setSortBy(e.target.value)}>
+        <option value="input">Sort by input order</option>
+        <option value="description">Sort by description</option>
+        <option value="packed">Sort by packed status</option>
+      </select>
+    </div>
+  );
+}
